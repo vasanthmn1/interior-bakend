68083c537f87a8cd6bb36b58c76678cab8298d61
vasanthmn1 <mnvasanth4@gmail.com>
2023-04-22 19:07:27 +0530

    change

--- controller/dataCtrl.js
+++ controller/dataCtrl.js
@@ -1,11 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Data = require('../model/dataMode');
-// const User = require('../model/usermodel');
 
-// const getGoals = asyncHandler(async (req, res) => {
-//     const goals = await Data.find({ user: req.user.id });
-//     res.status(200).json(goals);
-// });
 const getDataAll = asyncHandler(async (req, res) => {
 
     const data = await Data.find({ Data });
@@ -13,18 +8,7 @@ const getDataAll = asyncHandler(async (req, res) => {
 });
 const adddata = asyncHandler(async (req, res) => {
 
-    const { name, email, city, number, message } = req.body
 
-
-    // if (!name || !email || !city || !number || !message) {
-    //     res.status(400);
-    //     throw new Error('Pls fill all field');
-    // }
-    // name: req.body.name,
-    // email: req.body.email,
-    // city: req.body.city,
-    // number: req.body.number,
-    // message: req.body.message,
     const addData = await Data.create({
         ...req.body
     });
@@ -38,13 +22,6 @@ const deletedata = asyncHandler(async (req, res) => {
         throw new Error('user not found');
     }
 
-    // if (!req.user) {
-    //     res.status(401);
-    //     throw new Error('User not found');
-    // }
-
-
-    // await user.remove();
 
     res.status(200).json({ id: req.params.id });
 });
